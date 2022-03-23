18cf38336eaeb00277876143aeb3f340ea0d34f6
Wouter Roux <wouter.roux@resmed.com>
2022-03-22 20:03:08 -0700

    zprofile update

--- .bootstrap.sh
+++ .bootstrap.sh
@@ -9,8 +9,8 @@ echo "Ensuring Homebrew is installed and updated"
 if ! command -v brew > /dev/null; then
   echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> ~/.zprofile
-  eval $(/opt/homebrew/bin/brew shellenv)
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 brew update
