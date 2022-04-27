2a82588034720c6bddfd6915f17d88a666c5ced6
Wouter Roux <wouter.roux@gmail.com>
2022-04-27 16:42:13 -0700

    Pyenv and pip setup

---
+++ run_once_11-python-setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+default_python_version="3.7.13"
+python_versions=(
+  "$default_python_version"
+)
+python_packages=(
+  poetry
+)
+
+for version in "${python_versions[@]}"; do
+  pyenv install -s "$version"
+done
+
+pyenv global "$default_python_version"
+
+if ! command -v pip >/dev/null 2>&1; then
+  if [[ -d "${HOME}/.pyenv/shims" ]]; then
+    export PATH="${HOME}/.pyenv/shims:${PATH}"
+  fi
+fi
+
+pip install "${python_packages[@]}"
+pyenv rehash || true
